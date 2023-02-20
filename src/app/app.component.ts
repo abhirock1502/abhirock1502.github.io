efc4da25d2f6758d1ba0aba5226b1bb0a96304dc
abhimanyu <abhimanyu@clevertap.com>
2023-02-20 16:37:01 +0530

    Added On user login feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,10 @@ export class AppComponent implements OnInit {
   //    }, 2000);
   // }
   title = 'clevertap-sdk-angular';
+  emailUserName : String = "";
+  userIdentity : String = "";
+  constantPP : String = "";
+  constantPPIdentity : string = "";
 
   ngOnInit() {
     clevertap.spa = true;
@@ -29,8 +33,49 @@ export class AppComponent implements OnInit {
       "Price": 59.99,
       "Date": new Date()
     }); // Replace Payload as per your event schema and design
+    console.log('CleverTap Identity is ',clevertap.getCleverTapID())
   }
   onClickOULSame(){
-    //need to write login here for On user login
+    clevertap.onUserLogin.push({
+        Site :{
+          // Email: `${Math.ceil(Math.random() * 100012)}@gmail.com`
+          Email: `${this.emailUserName}@gmail.com`
+        }
+    })
+    console.log('User logged Sucessfully ',this.emailUserName);
+  }
+
+  onClickOULIdentitySame(){
+    console.log('On User login identitry button is clicked')
+    clevertap.onUserLogin.push({
+      Site :{ 
+        // Email: `${Math.ceil(Math.random() * 100012)}@gmail.com`
+        Identity: `${this.userIdentity}@gmail.com`}
+    })
+    console.log('User identity logged Sucessfully', this.userIdentity);
+  }
+
+  onClickPP() {
+    clevertap.profile.push({
+      Site :{
+        Email: `as${Math.ceil(Math.random() * 10002320)}@gmail.com`}
+    });
+    console.log('Profile pushed Sucessfully', this.emailUserName);
+  }
+
+  onClickPPSame(){
+    clevertap.profile.push({
+      Site :{
+        Email: `${this.constantPP}@gmail.com`}
+    });
+    console.log('Profile pushed Sucessfully', `${this.constantPP}@gmail.com`);
+  }
+
+  onClickPPIdentitySame(){
+    clevertap.profile.push({
+      Site :{
+        Identity: `${this.constantPPIdentity}`}
+    });
+    console.log('PP identity done Sucessfully for ', this.constantPPIdentity);
   }
 }
